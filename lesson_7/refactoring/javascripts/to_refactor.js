55344942d5d1d52310594b0535db476d946b46cc
Menachem Korf <menachemkorf@gmail.com>
2017-04-27 20:32:43 +1000

    complete refactoring exercise

---
+++ lesson_7/refactoring/javascripts/to_refactor.js
@@ -0,0 +1,88 @@
+$(function() {
+  $("nav a").on("mouseenter", function() {
+    $(this).next("ul").addClass('visible');
+  });
+
+  $("nav").on("mouseleave", function() {
+    $(this).find("ul ul").removeClass('visible');
+  });
+
+  $(".button, button").on("click", function(e) {
+    e.preventDefault();
+
+    $(this).addClass("clicked");
+  });
+
+  $(".toggle").on("click", function(e) {
+    e.preventDefault();
+
+    $(this).next(".accordion").toggleClass("opened");
+  });
+
+  function ccNumberTotal(cc_number) {
+    var odd_total = 0;
+    var even_total = 0;
+
+    cc_number = cc_number.split("").reverse();
+    for (var i = 0, len = cc_number.length; i < len; i++) {
+      if (i % 2 == 1) {
+        cc_number[i] = (+cc_number[i] * 2) + "";
+        if (cc_number[i].length > 1) {
+          cc_number[i] = +cc_number[i][0] + +cc_number[i][1];
+        }
+        else {
+          cc_number[i] = +cc_number[i];
+        }
+        odd_total += cc_number[i];
+      }
+      else {
+        even_total += +cc_number[i];
+      }
+    }
+
+    return odd_total + even_total;
+  }
+
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    var cc_number = $(this).find("[type=text]").val();
+    var total = ccNumberTotal(cc_number);
+    var isValid = total % 10 === 0;
+
+    $(this).find(".success").toggle(isValid);
+    $(this).find(".error").toggle(!isValid);
+
+    // if (total % 10 == 0) {
+    //   $(this).find(".success").show();
+    //   $(this).find(".error").hide();
+    // }
+    // else {
+    //   $(this).find(".error").show();
+    //   $(this).find(".success").hide();
+    // }
+  });
+
+  $("ul a").on("click", function(e) {
+    e.preventDefault();
+
+    var birthstones = {
+      "January": "garnet",
+      "February": "amethyst",
+      "March": "aquamarine or bloodstone",
+      "April": "diamond",
+      "May": "emerald",
+      "June": "pearl, moonstone, or alexandrite",
+      "July": "ruby",
+      "August": "peridot",
+      "September": "sapphire",
+      "October": "opal or tourmaline",
+      "November": "topaz or citrine",
+      "December": "turquoise, zircon, or tanzanite"
+    }
+
+    var month = $(this).text();
+    var $stone = $("#birthstone");
+
+    $stone.text("Your birthstone is " + birthstones[month]);
+  });
+});
