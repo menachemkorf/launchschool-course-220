26455a31d8daa617f639e002544e2ca1582dda9e
Menachem Korf <menachemkorf@gmail.com>
2017-04-28 11:37:26 +1000

    add all index files, complete animate_comic exercise

---
+++ lesson_8/animate_comic/comic.js
@@ -0,0 +1,26 @@
+$(function() {
+  var $blinds = $('[id^=blind]');
+
+  function startAnimation() {
+    $blinds.each(function(i) {
+      var $blind = $blinds.eq(i);
+      var delay = 1500;
+      var speed = 250;
+
+      $blind.delay(delay * i + speed).animate({
+        top: '+=' + $blind.height(),
+        height: 0
+      }, speed);
+    });
+  }
+
+  $('a').click(function(e) {
+    e.preventDefault();
+
+    $blinds.finish();
+    $blinds.removeAttr('style');
+    startAnimation();
+  });
+
+  startAnimation();
+});
