9f47850d9c6f4b884711410674d38e71c2163ee2
Menachem Korf <menachemkorf@gmail.com>
2017-05-04 21:35:21 +1000

    complete localstorage project

---
+++ lesson_10/localstorage/scripts/localstorage.js
@@ -0,0 +1,41 @@
+$(function() {
+  var $tabs = $('nav a');
+  var $articles = $('#tabs article');
+  var tab = localStorage.getItem('tab');
+  var color = localStorage.getItem('color');
+  var note = localStorage.getItem('note');
+
+  function showActiveTab(tab) {
+    $tabs.removeClass('active').eq(tab).addClass('active');
+    $articles.hide().eq(tab).show();
+  }
+
+  function colorBody(color) {
+    $('body').css('background-color', color);
+  }
+
+  showActiveTab(tab);
+  colorBody(color);
+  $('[value="'+ color +'"]').prop('checked', true);
+  // console.log($('input[value="'+ color +'"]'));
+  $('textarea').val(note);
+
+  $('nav').on('click', 'a', function(e) {
+    e.preventDefault();
+
+    tab = $(this).closest('li').index();
+    showActiveTab(tab);
+    localStorage.setItem('tab', tab);
+  });
+
+  $(':radio').on('change', function(e) {
+    color = $(this).val();
+    colorBody(color);
+    localStorage.setItem('color', color);
+  });
+
+  $(window).on('unload', function(e) {
+    localStorage.setItem('note', $('textarea').val());
+  });
+
+});
